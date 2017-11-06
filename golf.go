3c7e3444acc8be2ed957984dd6202847b50ecb40
Henrique Lopes <riquellopes@gmail.com>
2017-11-06 20:00:12 -0200

    init refac

--- golf.go
+++ golf.go
@@ -1,44 +1,51 @@
 package main
 
 import (
-   "net/http"
-   "github.com/labstack/echo"
-   "github.com/labstack/echo/engine/standard"
+	"net/http"
+
+	"github.com/labstack/echo"
 )
 
 type SearchExemple struct {
-    One string `json:"exampleONE"`
-    Two string `json:"exampleTWO"`
+	One string `json:"exampleONE"`
+	Two string `json:"exampleTWO"`
 }
 
 type Home struct {
-    About string `json:"about"`
-    Contact string `json:"contact"`
-    Project string `json:"project"`
-    SearchExemple `json:"search"`
+	About         string `json:"about"`
+	Contact       string `json:"contact"`
+	Project       string `json:"project"`
+	SearchExemple `json:"search"`
 }
 
 func main() {
-    app := echo.New()
+	app := echo.New()
 
-    app.GET("/", func(c echo.Context) error {
-        home := Home{"Recupera informações sobre proventos dos fii",
-                     "http://www.henriquelopes.com.br",
-                     "https://github.com/riquellopes/fii",
-                     SearchExemple{"curl -X GET http://vast-lake-49104.herokuapp.com/api/",
-                                   "curl -X GET http://vast-lake-49104.herokuapp.com/api/CNES11B"}}
-        return c.JSON(http.StatusOK, home)
-    })
+	app.GET("/", func(c echo.Context) error {
+		home := Home{"Recupera informações sobre proventos dos fii",
+			"http://www.henriquelopes.com.br",
+			"https://github.com/riquellopes/fii",
+			SearchExemple{"curl -X GET http://vast-lake-49104.herokuapp.com/api/",
+				"curl -X GET http://vast-lake-49104.herokuapp.com/api/CNES11B"}}
+		return c.JSON(http.StatusOK, home)
+	})
 
-    app.GET("/scrap", func(c echo.Context) error {
-        return c.String(http.StatusOK, "scrap")
-    })
+	app.GET("/api/", allFII("list alls"))
+	app.GET("/api/:codigo", fiiByCode("one"))
 
-    app.GET("/:codigo", func(c echo.Context) error {
-        codigo := c.Param("codigo")
+	app.Logger.Fatal(app.Start(":5000"))
+}
 
-        return c.String(http.StatusOK, codigo)
-    })
+func allFII(name string) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.String(http.StatusOK, name)
+	}
+}
 
-    app.Run(standard.New(":5000"))
+func fiiByCode(name string) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		codigo := c.Param("codigo")
+
+		return c.String(http.StatusOK, codigo)
+	}
 }
