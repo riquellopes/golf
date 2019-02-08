b6865d35e015b9f8194cf717a3793e4f0e07d605
Henrique Lopes <riquellopes@gmail.com>
2019-02-08 21:27:06 -0200

    crawler

--- crawler/scrapper.go
+++ crawler/scrapper.go
@@ -1,1 +1,52 @@
 package crawler
+
+import (
+	"os"
+	"strings"
+
+	"github.com/gocolly/colly"
+)
+
+// FII -
+type FII struct {
+	Code         string
+	BaseDate     string
+	BasePrice    string
+	RealYield    string
+	PaymentDate  string
+	PercentYield string
+	Observations string
+}
+
+// Do -
+func Do() []FII {
+	Collector := colly.NewCollector(
+		colly.CacheDir("./.cache"),
+		colly.UserAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"),
+	)
+
+	fiis := make([]FII, 0, 150)
+
+	Collector.OnHTML("table tr", func(e *colly.HTMLElement) {
+		ch := e.DOM.Children()
+
+		if strings.Contains(ch.Eq(0).Text(), "Código") {
+			return
+		}
+
+		fii := FII{
+			Code:         ch.Eq(0).Text(),
+			BaseDate:     ch.Eq(1).Text(),
+			BasePrice:    ch.Eq(2).Text(),
+			RealYield:    ch.Eq(3).Text(),
+			PaymentDate:  ch.Eq(4).Text(),
+			PercentYield: ch.Eq(5).Text(),
+			Observations: ch.Eq(6).Text(),
+		}
+
+		fiis = append(fiis, fii)
+	})
+
+	Collector.Visit(os.Getenv("ENDPOINT"))
+	return fiis
+}
