5a40bd0bc7ab42784b63b1ea7a67f75ed0ad1ccf
Henrique Lopes <riquellopes@gmail.com>
2019-02-09 16:24:40 -0200

    starting test

---
+++ api/views_test.go
@@ -0,0 +1,25 @@
+package api
+
+// import (
+// 	"net/http/httptest"
+// 	"testing"
+//
+// 	"github.com/labstack/echo"
+// 	"github.com/riquellopes/golf/crawler"
+// 	"github.com/stretchr/testify/assert"
+// )
+//
+// func Test_(t *testing.T) {
+// 	e := echo.New()
+// 	req := httptest.NewRequest(echo.GET, "/api/BCFF11", nil)
+// 	rec := httptest.NewRecorder()
+// 	context := e.NewContext(req, rec)
+//
+// 	fii := []crawler.FII{
+// 		crawler.FII{
+// 			Code: "BCFF11",
+// 		},
+// 	}
+//
+// 	assert.NoError(t, CodeF(fii)(context))
+// }
