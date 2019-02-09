4632e4d36b2c436e6032e899faa6bea5f63b9dc3
Henrique Lopes <riquellopes@gmail.com>
2019-02-09 17:10:36 -0200

    adding a new test and including the test package to echo

--- api/views_test.go
+++ api/views_test.go
@@ -1,25 +1,25 @@
 package api
 
-// import (
-// 	"net/http/httptest"
-// 	"testing"
-//
-// 	"github.com/labstack/echo"
-// 	"github.com/riquellopes/golf/crawler"
-// 	"github.com/stretchr/testify/assert"
-// )
-//
-// func Test_(t *testing.T) {
-// 	e := echo.New()
-// 	req := httptest.NewRequest(echo.GET, "/api/BCFF11", nil)
-// 	rec := httptest.NewRecorder()
-// 	context := e.NewContext(req, rec)
-//
-// 	fii := []crawler.FII{
-// 		crawler.FII{
-// 			Code: "BCFF11",
-// 		},
-// 	}
-//
-// 	assert.NoError(t, CodeF(fii)(context))
-// }
+import (
+	"testing"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/test"
+	"github.com/riquellopes/golf/crawler"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_should_not_get_error(t *testing.T) {
+	e := echo.New()
+	req := test.NewRequest(echo.GET, "/api/BCFF11", nil)
+	rec := test.NewResponseRecorder()
+	context := e.NewContext(req, rec)
+
+	fii := []crawler.FII{
+		crawler.FII{
+			Code: "BCFF11",
+		},
+	}
+
+	assert.NoError(t, CodeF(fii)(context))
+}
